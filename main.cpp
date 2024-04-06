5efc510eabd6d7769eb93360019f6e718049f566
Raymond Langer <raymolanger@gmail.com>
2024-04-06 13:59:13 -0400

    Removed broadcasting, ensured random number generator and sorting were working properly

--- main.cpp
+++ main.cpp
@@ -5,7 +5,7 @@
 
 int main(){
     int rank, size;
-    int N = 100, Z = 10;
+    int N = 50, Z = 10;
 
     MPI_Init(nullptr,nullptr);
 
@@ -16,45 +16,55 @@ int main(){
     std::mt19937 gen(std::random_device{}());
 
     for (int j = 0; j < N/Z; j ++){
-        rvector<int> arr_sizes(size-1);
-        rvector<int> offsets(size-1);
+        rvector<int> arr_sizes(size);
+        rvector<int> offsets(size);
         rvector<double> rands(Z);
-
-        if (rank = 0){
+        // rvector<double> recv_vals;
+        if (rank == 0){
             for(int i = 0; i < Z; i++){
                 rands[i] = uniform(gen);
             }
+            
             std::sort(rands.begin(),rands.end());
-
+            std::cout << rands << "\n";
             int counter = 0;
+            double limit = (counter+1.0)/size;
             int data_init = 0;
             for (int i = 0; i < Z; i++){
-                if(rands[i] > (counter+1)/size){
-                    if (counter = 0){
-                        int size_0 = i-1;
-                        rvector<double> recv_vals(size_0);
-                        for (int i = 0; i < size_0; i++){
-                            recv_vals[i] = rands[i];
-                        }
-                        counter += 1;
-                        data_init = i;
-                    }else{
-                        arr_sizes[counter-1] = i-data_init-1;
-                        offsets[counter - 1] = data_init;
-                        counter += 1;
-                        data_init = i;
-                    }
+
+                if(rands[i] > limit){
+                    // if (counter = 0){
+                    //     int size_0 = i-1;
+                    //     rvector<double> recv_vals(size_0);
+                    //     for (int i = 0; i < size_0; i++){
+                    //         recv_vals[i] = rands[i];
+                    //     }
+                    //     counter += 1;
+                    //     data_init = i;
+                    // }else{
+                    arr_sizes[counter] = i-data_init;
+                    offsets[counter] = data_init;
+                    counter += 1;
+                    limit = (counter+1.0)/size;
+                    data_init = i;
+                    // }
                 }
             }
+            arr_sizes[counter] = rands.size()-data_init;
+            offsets[counter] = data_init;
+            std::cout << arr_sizes << "\n";
+            std::cout << offsets << "\n";
         }
-        MPI_Bcast(arr_sizes.data(), size-1, MPI_INT, 0, MPI_COMM_WORLD);
+
+        MPI_Bcast(arr_sizes.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
         
-        if(rank!=0){
-            rvector<double> recv_vals(arr_sizes[rank-1]);
-        }
-        MPI_Bcast(offsets.data(), size-1, MPI_INT, 0, MPI_COMM_WORLD);
-        MPI_Scatterv(rands.data(), arr_sizes.data(), offsets.data(), MPI_DOUBLE, recv_vals.data(), arr_sizes.data(), 0, MPI_COMM_WORLD);
-        std::cout << "Rank " << rank << " received " << recv_vals << ".\n" 
+        // if(rank!=0){
+        //     rvector<double> recv_vals(arr_sizes[rank-1]);
+        // }
+        MPI_Bcast(offsets.data(), size, MPI_INT, 0, MPI_COMM_WORLD);
+        // MPI_Scatterv(rands.data(), arr_sizes.data(), offsets.data(), MPI_DOUBLE, recv_vals.data(), arr_sizes.data(), 0, MPI_COMM_WORLD);
+        // std::cout << "Rank " << rank << " received " << recv_vals << ".\n" 
+        // std::cout << "Rank " << rank << " received " << arr_sizes[rank] << ", " << offsets[rank] << ".\n";
     }
     MPI_Finalize();
     return 0;
